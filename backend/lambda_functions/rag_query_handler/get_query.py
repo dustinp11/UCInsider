b7e60d8c95810c90f66a5d9a3e1042f4903affa2
Dustin Pham <dustpham123@gmail.com>
2025-05-10 18:57:51 -0700

    Implemented RAG with claude

--- backend/lambda_functions/rag_query_handler/get_query.py
+++ backend/lambda_functions/rag_query_handler/get_query.py
@@ -1,44 +1,43 @@
 import boto3
 import json
+from botocore.client import Config
+from botocore.exceptions import ClientError
 
-# 1) Use us-west-2
-bedrock = boto3.client(
-    'bedrock-runtime',
-    region_name='us-west-2'
 
-)
 
-# 2) Build your request body
-body = json.dumps({
-    "anthropic_version": "bedrock-2023-05-31",
-    "max_tokens": 1000,
-    "messages": [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "Hello What is happiness?"
-                }
-            ]
+def generate_response(user_query):
+    """Takes in a user query and has Claude 3.7 sonnet generate a response based on the given context (reddit posts).
+    
+    Args:
+        user_query (str): The give user input as a string
+    Returns: 
+        Nothing
+    """
+    boto_session = boto3.Session()
+    aws_region = boto_session.region_name
+    bedrock_config = Config(connect_timeout=120, read_timeout=120, retries={'max_attempts': 0})
+    bedrock_agent_client = boto3.client(
+        'bedrock-agent-runtime', 
+        aws_access_key_id=..., 
+        aws_secret_access_key=...,
+        region_name='us-west-2',
+        config=bedrock_config)
+    preprompt = "Every response you should provide a description of the class first. Then talk about the content of the class. Then give pros and cons of the class and talk about the different professors if available. End with final verdict."
+    final_query = preprompt + user_query 
+    
+    response = bedrock_agent_client.retrieve_and_generate(
+        input={
+            'text': final_query
+        },
+        retrieveAndGenerateConfiguration={
+            'type': 'KNOWLEDGE_BASE',
+            'knowledgeBaseConfiguration': {
+                'knowledgeBaseId': ...,
+                'modelArn': ...
+            }
         }
-    ]
-})
+    )
+    print("User query: ", user_query, '\n')
+    print("Final reply:\n", response['output']['text'])
 
-# 3) Point modelId at your us-west-2 inference profile ARN
-modelId = ...
-
-accept      = 'application/json'
-contentType = 'application/json'
-
-# 4) Invoke and print
-response = bedrock.invoke_model(
-    modelId=modelId,
-    contentType=contentType,
-    accept=accept,
-    body=body
-)
-
-result = json.loads(response['body'].read())
-assistant_text = result['content'][0]['text']
-print(assistant_text)
+generate_response()
